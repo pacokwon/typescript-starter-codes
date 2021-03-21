15580049616a9a334b01cb6b5726a5e71ca16b4f
pacokwon <haechank@gmail.com>
2021-03-21 15:45:44 +0900

    misc: add package-lock and format file

--- react/src/components/ComplicatedCounter.tsx
+++ react/src/components/ComplicatedCounter.tsx
@@ -1,27 +1,27 @@
 import React, { useState } from 'react'
 
 const MinusButton = ({ setCount, children }) => {
-    const handleClick = () => setCount(count => count - 1);
+  const handleClick = () => setCount(count => count - 1);
 
-    return <button onClick={handleClick}>{children}</button>
+  return <button onClick={handleClick}>{children}</button>
 }
 
 const PlusButton = ({ setCount, children }) => {
-    const handleClick = () => setCount(count => count + 1);
+  const handleClick = () => setCount(count => count + 1);
 
-    return <button onClick={handleClick}>{children}</button>
+  return <button onClick={handleClick}>{children}</button>
 }
 
 const ComplicatedCounter = ({ initialValue = 0 }) => {
-    const [count, setCount] = useState(initialValue);
+  const [count, setCount] = useState(initialValue);
 
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <MinusButton setCount={setCount}>-</MinusButton>
-            {count}
-            <PlusButton setCount={setCount}>+</PlusButton>
-        </div>
-    )
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <MinusButton setCount={setCount}>-</MinusButton>
+      {count}
+      <PlusButton setCount={setCount}>+</PlusButton>
+    </div>
+  )
 }
 
 export default ComplicatedCounter;
